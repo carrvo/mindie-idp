290499debd9b77a1530671d7afa1d72d9182cd35
carrvo <carrvo@gmail.com>
2025-01-24 13:48:08 -0700

    parent directories to SQLite requrie write access

--- setup.bash
+++ setup.bash
@@ -7,7 +7,7 @@ sudo chmod g+rwx /var/lib/selfauth/
 # MinToken
 sudo mkdir /var/lib/php-mintoken
 sudo chgrp www-data /var/lib/php-mintoken/
-sudo chmod g+rx /var/lib/php-mintoken/
+sudo chmod g+rwx /var/lib/php-mintoken/
 sudo sqlite3 /var/lib/php-mintoken/tokens.sqlite3 < ./mintoken/schema.sql
 sudo chgrp www-data /var/lib/php-mintoken/tokens.sqlite3
 sudo chmod g+rw /var/lib/php-mintoken/tokens.sqlite3
