1b28992677aa2d172a02db8ca716676eb0a7ec7b
carrvo <carrvo@gmail.com>
2025-01-28 16:53:48 -0700

    fix schema path

--- setup.bash
+++ setup.bash
@@ -4,7 +4,7 @@
 sudo mkdir /var/lib/selfauth
 sudo chgrp www-data /var/lib/selfauth/
 sudo chmod g+rwx /var/lib/selfauth/
-sudo sqlite3 /var/lib/selfauth/multiuser.sqlite3 < ./schema.sql
+sudo sqlite3 /var/lib/selfauth/multiuser.sqlite3 < ./selfauth/schema.sql
 sudo chgrp www-data /var/lib/selfauth/multiuser.sqlite3
 sudo chmod g+rw /var/lib/selfauth/multiuser.sqlite3
 # MinToken
