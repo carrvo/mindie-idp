29064b72f244bcad0be5d9c9385c1ece75caf9af
carrvo <carrvo@gmail.com>
2025-12-26 03:15:13 -0700

    merge SelfAuth and MinToken

--- selftoken.php
+++ selftoken.php
@@ -1,7 +1,11 @@
 <?php
 declare(strict_types=1);
 
+require __DIR__ . '/selfauth/index.php';
 require __DIR__ . '/mintoken/endpoint.php';
+
+$app_url = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST']
+  . preg_replace('/token.*$/', '', $_SERVER['REQUEST_URI']);
 
 $method = get_method();
 if ($method === 'GET') {
@@ -28,8 +32,22 @@ if ($method === 'GET') {
     }
     // else is a POST+authorization request
     $request = get_request();
-    $endpoints = getTrustedEndpoints();
-    $info = verify_against_trusted($request, $endpoints);
+
+
+    // query SelfAuth library directly
+    $configs = load_user_config($app_url);
+
+    // Scan through the existing users then
+    // Exit if there are errors in the client supplied data.
+    $user_verified = user_verify($request['code'], $request['redirect_uri'], $request['client_id'], $configs);
+    if ($user_verified === false) {
+        invalidRequest('Verification Failed: Given Code Was Invalid');
+    }
+
+    $info = get_response($request['code'], $user_verified);
+    // end SelfAuth library
+
+
     $token = storeToken($info['me'], $request['client_id'], $info['scope']);
     header('HTTP/1.1 200 OK');
     header('Content-Type: application/json;charset=UTF-8');
